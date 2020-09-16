b0e5c39bd61f70166706014e6783d661dd086fef
Doris8462 <liandou123>
2020-09-16 08:16:36 +0800

    [haungyunjie] feat:fix request.test

--- src/__test__/request.test.js
+++ src/__test__/request.test.js
@@ -1,17 +1,20 @@
+// eslint-disable-next-line no-unused-vars
 import * as request from "../javascript/request";
 
 jest.mock("../javascript/request.js");
 describe("request", () => {
+  const getUser = jest.fn().mockImplementation(() => true);
+  const getEducation = jest.fn().mockImplementation(() => true);
   test("should get user info when getUser called", async () => {
-    request.getUser.mockImplementation(() => {});
-    request.getUser(1);
-    expect(request.getUser).toHaveBeenCalled();
-    expect(request.getUser).toHaveBeenCalledTimes(1);
+    getUser.mockImplementation(() => {});
+    getUser(1);
+    expect(getUser).toHaveBeenCalled();
+    expect(getUser).toHaveBeenCalledTimes(1);
   });
   test("should get Educations info when getEducation called", async () => {
-    request.getEducation.mockImplementation(() => {});
-    request.getEducation(1);
-    expect(request.getEducation).toHaveBeenCalled();
-    expect(request.getEducation).toHaveBeenCalledTimes(1);
+    getEducation.mockImplementation(() => {});
+    getEducation(1);
+    expect(getEducation).toHaveBeenCalled();
+    expect(getEducation).toHaveBeenCalledTimes(1);
   });
 });
